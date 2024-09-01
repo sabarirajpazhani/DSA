c4abd85d5b4ab64c03495aed9ac252399defff7a
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-09-01 22:12:06 +0530

    Ouput and link

--- Longest_Subarray_with_sum_k/Better_Approach.java
+++ Longest_Subarray_with_sum_k/Better_Approach.java
@@ -24,3 +24,24 @@ public class Better_Approach {
 	}
 }	
 
+
+// Sample Output 1 :
+// 2
+// 3
+// Explanation Of Sample Input 1 :
+// For the first case:
+// There are two subarrays with sum = 5, [2, 3] and [5]. Hence the length of the longest subarray is 2.
+
+// For the second case:
+// Longest subarray with sum = 1 is [1, -1, 1].
+// Sample Input 2 :
+// 2
+// 3 4
+// 1 1 1
+// 3 2
+// -50 0 52
+// Sample Output 2 :
+// 0 
+// 3
+
+//LINK: https://naukri.com/code360/problems/longest-subarray-with-sum-k_5713505?interviewProblemRedirection=true&search=Longest%20Subarray%20With%20Sum%20k&leftPanelTabValue=PROBLEM
